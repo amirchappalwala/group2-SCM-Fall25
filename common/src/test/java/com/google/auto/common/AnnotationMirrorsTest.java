36110dc660d429d38f50f3f3b02df94c05fba8b1
Hamza Asghar Khan <63193196+hamzaasgharkhan@users.noreply.github.com>
2025-11-03 01:35:46 -0500

    Unused Import Removed from AnnotationMirrorsTest.java

--- common/src/test/java/com/google/auto/common/AnnotationMirrorsTest.java
+++ common/src/test/java/com/google/auto/common/AnnotationMirrorsTest.java
@@ -18,7 +18,6 @@ package com.google.auto.common;
 import static com.google.auto.common.AnnotationMirrorsTest.SimpleEnum.BLAH;
 import static com.google.auto.common.AnnotationMirrorsTest.SimpleEnum.FOO;
 import static com.google.common.collect.Iterables.getOnlyElement;
-import static com.google.common.truth.Truth.assertThat;
 import static com.google.testing.compile.CompilationSubject.assertThat;
 import static org.junit.Assert.fail;
 
